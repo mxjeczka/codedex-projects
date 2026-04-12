bb4f176b376c32395bc3d30018b9a10ef885527e
Maja <majaperz15@gmail.com>
2026-04-12 18:31:16 +0200

    Entwurf entfernt

--- Java/beginnerCourse/finalProject/MixMagic/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.SwingUtilities;
-import gui.PotionMixerWindow;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(PotionMixerWindow::new);
-    }
-}
