a0c0139b266bb8e7a0dc82f2b60d7da39848c97e
3perz <maja.perz@studium.uni-hamburg.de>
2026-08-09 20:11:27 +0200

    first drafts of the files

---
+++ Javascript/beginnerCourse/checkpointProject/script.js
@@ -0,0 +1,12 @@
+VANTA.FOG({
+    el: "#fog",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x640186,
+    midtoneColor: 0xc0077,
+    lowlightColor: 0xb30d8b,
+    baseColor: 0x0
+});
